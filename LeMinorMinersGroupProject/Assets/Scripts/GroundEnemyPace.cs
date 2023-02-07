a835525744e1108aaa3bec1cc7e8dd70e4d1e6a8
Wizrd101 <simonhenderson100@gmail.com>
2023-02-07 08:54:42 -0800

    EnemyMove done (- animations)
    Finished up, the enemy will flip when it encounters a ledge, and the whole sprite will flip in general when it turns (so that I don't have to set that up later when I get the animations)
    
    The last thing I need is the animations for the enemies. Send me some animations and I'll do that when I get them.

--- LeMinorMinersGroupProject/Assets/Scripts/GroundEnemyPace.cs
+++ LeMinorMinersGroupProject/Assets/Scripts/GroundEnemyPace.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+// Also should have two colliders: one for the main body (the normal one)
+// and a trigger collider just in front of where the enemy is facing
 
 public class GroundEnemyPace : MonoBehaviour
 {
@@ -20,6 +22,8 @@ public class GroundEnemyPace : MonoBehaviour
 
     int dir = 1;
 
+    bool goingOnGround = true;
+
     Rigidbody2D rb;
     Animator anim;
 
@@ -35,45 +39,84 @@ public class GroundEnemyPace : MonoBehaviour
     {
         Vector3 playerPosition = player.transform.position;
         Vector2 chaseDir = new Vector2 (playerPosition.x - transform.position.x, playerPosition.y - transform.position.y);
+        Vector3 scale = transform.localScale;
+        //If the player is in range, chase them
         if (chaseDir.magnitude <= chaseTriggerDist)
         {
             Vector2 chaseVector = new Vector2(playerPosition.x - transform.position.x, 0);
             chaseVector.Normalize();
-            rb.velocity = chaseVector * chaseSpeed;
-            Debug.Log("Chasing Player");
+            if (goingOnGround)
+            {
+                rb.velocity = chaseVector * chaseSpeed;
+            }
+            if (chaseVector.x > 0)
+            {
+                scale.x = 1;
+            }
+            else
+            {
+                scale.x = -1;
+            } 
+            transform.localScale = scale;
         }
+        // otherwise, patrol the area
         else
         {
             Vector2 distToStart = transform.position - startingPosition;
+            // If we're too far away from our patrol starting spot (from chasing) go back home
             if (distToStart.magnitude > patrolRange || goToStart)
             {
                 goToStart = true;
                 if (distToStart.x > 0)
                 {
                     dir = -1;
+                    scale.x = -1;
                 }
                 else
                 {
                     dir = 1;
+                    scale.x = 1;
                 }
+                transform.localScale = scale;
                 if (transform.position.x < startingX)
                 {
                     goToStart = false;
                 }
-                Debug.Log("Patroling, too far");
             }
+            // Otherwise, just patrol the area normally
             else
             {
                 if (transform.position.x < startingX || transform.position.x > startingX + patrolRange)
                 {
                     dir *= -1;
+                    scale.x = dir;
+                    transform.localScale = scale;
                 }
-                Debug.Log("Patroling, in range");
             }
-            transform.Translate(Vector2.right * patrolSpeed * Time.deltaTime * dir);
-            
+            // If enemy is on the ground, go forward
+            if (goingOnGround)
+            {
+                transform.Translate(Vector2.right * patrolSpeed * Time.deltaTime * dir);
+            }
+            // If not, turn around
+            else
+            {
+                dir *= -1;
+                scale.x = dir;
+                transform.localScale = scale;
+            }
         }
 
         //animator stuff
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        goingOnGround = true;
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        goingOnGround = false;
     }
 }
