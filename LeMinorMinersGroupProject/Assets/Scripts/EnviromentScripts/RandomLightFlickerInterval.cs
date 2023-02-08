59d96e08366721a60a53dd6765263524565b26a4
AradFox <115174667+AradFox@users.noreply.github.com>
2023-02-08 09:08:36 -0800

    readded ghost animations with the matching artstyle

--- LeMinorMinersGroupProject/Assets/Scripts/EnviromentScripts/RandomLightFlickerInterval.cs
+++ LeMinorMinersGroupProject/Assets/Scripts/EnviromentScripts/RandomLightFlickerInterval.cs
@@ -3,40 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-//public class LightFlicker : MonoBehaviour
-//{
-  //  float intensity;
-   // public float intensityMax;
-   // public float intensityMin;
-   // public float randomMin = 0f;
-   // public float randomMax = 10f;
+public class LightFlicker : MonoBehaviour
+{
+   float intensity;
+    public float intensityMax;
+    public float intensityMin;
+   public float randomMin = 0f;
+   public float randomMax = 10f;
 
 
-   // Light2D light1;
+    Light2D light1;
 
     // Start is called before the first frame update
-  //  void Start()
-  //  {
-  //      Invoke("RandomFlicker", Random.Range(randomMin, randomMax));
-  //      light1 = GetComponent<Light2D>();
-  //      intensity = light1.intensity;
-  //      intensityMax = (float)(intensity + 0.5);
-   //     intensityMin = (float)(intensity - 0.25);
+    void Start()
+    {
+        Invoke("RandomFlicker", Random.Range(randomMin, randomMax));
+        light1 = GetComponent<Light2D>();
+        intensity = light1.intensity;
+        intensityMax = (float)(intensity + 0.5);
+       intensityMin = (float)(intensity - 0.25);
 
-  //  }
+    }
 
-    // Update is called once per frame
-   // void Update()
-   // {
-   //     intensity = Random.Range(intensityMin, intensityMax);
-  //      light1.intensity = intensity;
+   // Update is called once per frame
+   void Update()
+    {
+        intensity = Random.Range(intensityMin, intensityMax);
+        light1.intensity = intensity;
 
-   // }
+   }
 
-  //  void RandomFlicker()
-  //  {
-  //      float randomTime = Random.Range(randomMin, randomMax);
+  void RandomFlicker()
+    {
+        float randomTime = Random.Range(randomMin, randomMax);
 
-   //     Invoke("RandomFlicker", randomTime);
-   // }
-//}
+       Invoke("RandomFlicker", randomTime);
+    }
+}
